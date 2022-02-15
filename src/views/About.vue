dec6e021e41c675940aedec3ddf51a3201d7b217
hanul-cha <ckgks3014@naver.com>
2022-02-15 16:35:03 +0900

    vue의 route도 react와 비슷하게 등록해서 사용하는구나

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>여긴 어바웃이야</h1>
+    </div>
+</template>
+<script>
+
+export default {};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>여긴 홈이야</h1>
+    </div>
+</template>
+<script>
+
+export default {};
+</script>
