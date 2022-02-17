cdb817e86f641bbb9dbc526c31f9ca2c75fefdaf
hanul-cha <ckgks3014@naver.com>
2022-02-17 15:18:44 +0900

    데이터 선언

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
     <div>
-        <h1>여긴 홈이야</h1>
+        <h1>{{title}} 인 {{title2}}</h1>
+        <input type="text" v-model="input1" />
+        <button type="button"></button>
+        <p>{{input1}}</p>
     </div>
 </template>
 <script>
 
-export default {};
+export default {
+    data() {
+        return {
+            title:"개발자",
+            title2:"차한울",
+            input1:"1234"
+        }
+    }
+};
 </script>
