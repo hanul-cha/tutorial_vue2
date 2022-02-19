822ef2a88bf390f39922344a94f73436e414eebd
hanul-cha <ckgks3014@naver.com>
2022-02-19 12:56:11 +0900

    v-for

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form action="">
-      <input type="text" v-model="name" />
+    <form action="" class="d-flex" @submit.prevent="onSubmit">
+      <div class="flex-grow-1 mr-3">
+        <input class="form-control" type="text" v-model="todo" placeholder="Type new todo" />
+      </div>
+      <div>
+        <button class="btn btn-primary" type="submit">ADD</button>
+      </div>
       <!-- value뿐 아니라 모든 속성을 바인딩 해줄수 있음 -->
-      <button class="btn btn-primary" @click="onSubmit">click</button>
     </form>
+    
+    <div 
+      class="card"
+      v-for="todo in todos" :key="todo.id"
+    >
+      <div class="card-body p-2">
+        {{ todo.subject }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,20 +27,23 @@ import { ref } from "@vue/reactivity";
 
 export default {
   setup() {
-    const name = ref("hanulCha");
+    const todo = ref("");
     //객채, 오브젝트는 reactive를 사용 프로퍼티로 바로 접근가능하다
-
-    /* const greeting = (name) => {
-      return "hello, " + name;
-    }
-    const greet = greeting(name); */
+    const todos = ref([
+      {id :1 , subject:"장보기"}
+    ])
 
     const onSubmit = () => {
-      console.log(name.value);
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value
+      });
+      todo.value = ""
     };
 
     return {
-      name,
+      todo,
+      todos,
       onSubmit,
     };
   },
@@ -46,6 +62,8 @@ vue3에선 이런 빈 플레그먼트 없이도 작성가능하다
 
 /* 
 v-on: == @
+e.preventDefalt할 필요 없이
+vue에선 이벤트를 걸때 속성에 prevent프로퍼티를 넣어주면됨
  */
 </script>
 
