d5582e832dea7f174f22a7ad3c010315e1b41a4e
hanul-cha <ckgks3014@naver.com>
2022-02-22 23:06:04 +0900

    연습

--- vue2/src/views/About.vue
+++ vue2/src/views/About.vue
@@ -1,9 +1,32 @@
 <template>
-    <div>
-        <h1>여긴 어바웃이야</h1>
+  <div>
+    <h1 @mouseover="onOver" @mouseleave="onLeave">
+      여긴 어바웃이야
+    </h1>
+    <div v-show="bool">
+      {{ title }}
     </div>
+  </div>
 </template>
 <script>
-
-export default {};
+export default {
+  data() {
+    return {
+      title: "개발자",
+      bool: false,
+      test:null
+    };
+  },
+  methods: {
+    onOver() {
+      this.test = setTimeout(() => {
+          this.bool = true;
+      }, 2000);
+    },
+    onLeave() {
+    clearTimeout(this.test)
+    this.bool = false;
+    },
+  },
+};
 </script>
