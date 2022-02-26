4faeca496a2794891318614b8dc44f1fd2570b80
차한울 <caroneul@datable.co.kr>
2022-02-26 12:49:21 +0900

    for

--- vue2/src/router.js
+++ vue2/src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "./views/Home";
 import About from "./views/About";
+import Todo from "./views/todo/TodoMain.vue";
 
 Vue.use(VueRouter);
 
@@ -10,7 +11,8 @@ const router = new VueRouter({
   routes: [
     { path: "/", component: Home },
     { path: "/about", component: About },
+    { path: "/todo", component: Todo },
   ],
 });
 
-export default router
+export default router;
---
+++ vue2/src/views/todo/TodoMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="test-center">TodoApp</h1>
+    <input type="text" class="w-100" placeholder="todo" />
+    <hr />
+    <Todo :key="todo.id" v-for="todo in todos" :todo="todo" />
+  </div>
+</template>
+
+<script>
+import Todo from "../../components/todo/Todo.vue";
+export default {
+  components: {
+    Todo,
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, text: "bay a car", checked: false },
+        { id: 2, text: "go alar", checked: false },
+      ],
+    };
+  },
+};
+</script>
